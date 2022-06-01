95e8781dcbfc4a6bd1c8ec65f8ee3eb45f672546
Kaydin0409 <klbond88@gmail.com>
2022-05-31 21:30:49 -0400

    1st commit

---
+++ src/gitReview/gitreview01.java
@@ -0,0 +1,12 @@
+package gitReview;
+
+public class gitreview01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I can add this code to github all by myself");
+
+	}
+
+}
